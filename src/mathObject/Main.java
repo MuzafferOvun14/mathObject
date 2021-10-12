a96c7ac794df1c6e27464ff6371b70f2d57c02dc
MuzafferOvun14 <ibmtalbilisim@gmail.com>
2021-10-12 15:36:30 +0300

    Initial commit

---
+++ src/mathObject/Main.java
@@ -0,0 +1,18 @@
+package mathObject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Math math=new Math();
+		System.out.println(math.sum(5, 13));
+		System.out.println(math.sub(5, 13));
+		System.out.println(math.divide(12,5));
+		System.out.println(math.getPi());
+		// pi*r*r   areaOfCircle
+		System.out.println(math.areaOfCircle(8.7));
+		System.out.println(math.factoriyel(5));
+		System.out.println(math.sumOfSmallNumbers(6));
+	}
+
+}
